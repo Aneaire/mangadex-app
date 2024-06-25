b6be78f5844917d11ef7eda4387e9f8a19f40633
LetsBeFriends01 <letsbefriend.companion@gmail.com>
2024-06-25 17:49:08 +0800

    try if cover art effect in deployment by CORS

--- src/components/TestApi.tsx
+++ src/components/TestApi.tsx
@@ -1,16 +1,26 @@
 "use client";
 
-import { testApiCall } from "@/lib/mangadex";
+import { useRef } from "react";
 import { Button } from "./ui/button";
 
 const TestApi = () => {
+  const image = useRef<any>();
+
   const fetchManga = async () => {
-    console.log("fetching manga");
-    testApiCall();
+    const res = await fetch(
+      "https://uploads.mangadex.org/covers/77bee52c-d2d6-44ad-a33a-1734c1fe696a/c384dedc-0f09-46ed-ae68-c81eb5eacdf4.jpg"
+    );
+    if (!res.ok) {
+      throw new Error("Image could not be fetched");
+    }
+    const imageBlob = await res.blob();
+    const imageURL = URL.createObjectURL(imageBlob);
+    image.current.src = imageURL;
   };
 
   return (
     <div>
+      <img className=" size-10" ref={image} src="" alt="" />
       Hi i am API Tester <Button onClick={fetchManga}>Fetch</Button>
     </div>
   );
